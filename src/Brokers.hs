103eaa9be33314007c523f6ffc206667239a3c62
b.egorov <b.egorov@tinkoff.ru>
2019-06-03 13:02:04 +0300

    Implement ticker

--- src/Brokers.hs
+++ src/Brokers.hs
@@ -2,9 +2,16 @@ module Brokers
     ( brokersFetcher
     ) where
 
-import Control.Concurrent (MVar, putMVar)
+import Control.Concurrent (MVar, putMVar, threadDelay)
 
 brokersFetcher :: MVar () -> IO ()
 brokersFetcher m = do
   putStrLn "started fetching brokers"
+  update 2000000
   putMVar m ()
+
+update :: Int -> IO ()
+update d = do
+  putStrLn "tick"
+  threadDelay d
+  update d
