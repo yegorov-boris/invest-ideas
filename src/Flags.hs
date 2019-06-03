f1627a155af6817e59d1676ff1a6a9f79728d2bb
b.egorov <b.egorov@tinkoff.ru>
2019-06-03 22:06:19 +0300

    Parse ideas polling interval

--- src/Flags.hs
+++ src/Flags.hs
@@ -7,6 +7,9 @@ module Flags
 
 import Options.Applicative
 import Data.Semigroup ((<>))
+import Text.Read (readEither)
+import Control.Monad ((>=>))
+import qualified Data.List.Safe as L
 
 data CliFlagsRaw = CliFlagsRaw
   { ideasUrlRaw             :: String
@@ -36,16 +39,32 @@ cliFlagsRaw = CliFlagsRaw
 parseCliFlags :: IO (Either String CliFlags)
 parseCliFlags = do
   raw <- execParser opts
-  return $ fromCliFlagsRaw raw
+  return $ fromRaw raw <$> parsePollingInterval (ideasPollingIntervalRaw raw)
   where
     opts = info (cliFlagsRaw <**> helper)
       ( fullDesc
-     <> progDesc "invest-ideas fetcher"
-     <> header "hello" )
+      <> progDesc "invest-ideas fetcher"
+      <> header "hello" )
+    fromRaw raw interval = CliFlags
+      { ideasUrl = ideasUrlRaw raw
+      , token = tokenRaw raw
+      , ideasPollingInterval = interval
+      }
 
-fromCliFlagsRaw :: CliFlagsRaw -> Either String CliFlags
-fromCliFlagsRaw raw = Right CliFlags
-  { ideasUrl = ideasUrlRaw raw
-  , token = tokenRaw raw
-  , ideasPollingInterval = 2000000
-  }
+-- TODO: lift
+parsePollingInterval :: String -> Either String Int
+parsePollingInterval s = (safeLast >=> parseLetter) s <*> (safeInit >=> safeRead) s >>= validate
+  where
+    safeLast = maybe (Left "failed to parse ideas-polling-interval") Right . L.last
+    parseLetter 'm' = Right (*1)
+    parseLetter 'h' = Right (*60)
+    parseLetter _   = Left "wrong format of ideas-polling-interval"
+    safeInit = maybe (Left "unsupported unit of time measurement in ideas-polling-interval") Right . L.init
+    safeRead = \v -> readEither v :: Either String Int
+
+-- TODO: guard
+validate :: Int -> Either String Int
+validate n
+  | n < 1     = Left "ideas-polling-interval should be at least 1m"
+  | n > 24*60 = Left "ideas-polling-interval should be no more than 24h"
+  | otherwise = Right $ 60*1000*1000*n
