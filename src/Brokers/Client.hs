28e6f035a216809e899667766ff642833704e7f3
b.egorov <b.egorov@tinkoff.ru>
2019-06-11 01:46:45 +0300

    Fix: cancel and retry an HTTP request on timeout

--- src/Brokers/Client.hs
+++ src/Brokers/Client.hs
@@ -7,28 +7,26 @@ import Data.ByteString.UTF8 (fromString)
 import qualified Data.ByteString.Char8 as B
 import Control.Exception (SomeException, handle)
 import Control.Conditional (if')
-import Control.Concurrent (MVar, newMVar, takeMVar, putMVar, forkIO, threadDelay, killThread)
+import Control.Concurrent (MVar, newEmptyMVar, readMVar, tryPutMVar, forkIO, threadDelay)
 import Flags (CliFlags, ideasURL, token, httpTimeout, httpMaxAttempts)
 import Utils (printWrap)
 
 -- TODO: read flags
--- TODO: chain conditions
 fetch :: CliFlags -> IO ()
 fetch cf = attemptFetch cf 1
 
 attemptFetch :: CliFlags -> Int -> IO ()
 attemptFetch cf currentAttempt = do
-  m <- newMVar False
-  id <- forkIO $ doFetch cf m currentAttempt
-  threadDelay $ httpTimeout cf
-  success <- takeMVar m
+  m <- newEmptyMVar
+  forkIO $ doFetch cf m currentAttempt
+  forkIO (threadDelay (httpTimeout cf) >> tryPutMVar m False >> return ())
+  success <- readMVar m
   if'
-    (success == True)
+    (success || (currentAttempt == maxAttempts))
     (return ())
-    (killThread id >> if'
-      (currentAttempt < httpMaxAttempts cf)
-      (attemptFetch cf (succ currentAttempt))
-      (return ()))
+    (attemptFetch cf (succ currentAttempt))
+  where
+    maxAttempts = httpMaxAttempts cf
 
 doFetch :: CliFlags -> MVar Bool -> Int -> IO ()
 doFetch cf m currentAttempt = do
@@ -37,18 +35,20 @@ doFetch cf m currentAttempt = do
   where
     url = fromString $ (ideasURL cf) ++ "/brokers?api_key=" ++ (token cf)
     processStatusCode response inputStream = do
-      takeMVar m
-      putMVar m True
-      let statusCode = getStatusCode response in if'
-        (statusCode == 200)
-        (putStrLn $ "finished fetching brokers, attempt " ++ show currentAttempt)
-        (printWrap ("failed to fetch brokers, attempt " ++ show currentAttempt ++ ": status code ") statusCode)
+      allowed <- tryPutMVar m True
+      if'
+        allowed
+        (let statusCode = getStatusCode response in if'
+          (statusCode == 200)
+          (printWrap "finished fetching brokers, attempt " currentAttempt)
+          (printWrap ("failed to fetch brokers, attempt " ++ show currentAttempt ++ ": status code ") statusCode))
+        (printWrap "failed to fetch brokers: timed out, attempt " currentAttempt)
 
 onErr :: Int -> SomeException -> IO ()
 onErr currentAttempt = printWrap ("failed to fetch brokers, attempt " ++ show currentAttempt ++ ": ")
 
 --import Data.Time.LocalTime (ZonedTime)
---
+
 --data Broker = Broker { externalID :: String
 --                     , source :: String
 --                     , name :: String
