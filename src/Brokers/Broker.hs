315f69d4bfe2aaf09933cb7f1f17259f5e6a4807
b.egorov <b.egorov@tinkoff.ru>
2019-06-13 19:54:55 +0300

    Parse response body

--- src/Brokers/Broker.hs
+++ src/Brokers/Broker.hs
@@ -6,32 +6,32 @@ module Brokers.Broker
 
 import GHC.Generics (Generic)
 import Data.Aeson (FromJSON)
--- import Data.Time.LocalTime (ZonedTime)
+import qualified Data.Text as T
 
 data Broker = Broker {
-  id :: Int
---                     , source :: String
---                     , name :: String
---                     , rating :: String
---                     , ideasCount :: Int
---                     , ideasPositive :: Int
---                     , description :: String
---                     , accuracy :: Double
---                     , profitableIdeasAvgYield :: Double
---                     , totalProfitableIdeas :: Int
---                     , unprofitableIdeasAvgYield :: Double
---                     , totalUnprofitableIdeas :: Int
---                     , bestIdeaExternalID :: String
---                     , newIdeasPerMonth :: Int
---                     , ideaAvgDaysLong :: Int
---                     , specializationResumeAsset :: String
---                     , specializationResumeCurrency :: String
---                     , specializationResumeDescription :: String
---                     , createdAt :: ZonedTime
---                     , updatedAt :: ZonedTime
---                     , isDeleted :: Bool
---                     , isVisibleMM :: Bool
---                     , isVisibleWM :: Bool
+    id                           :: Int
+  , name                         :: T.Text
+  , rating                       :: Int
+  , ideas_count                  :: T.Text
+  , ideas_positive               :: T.Text
+  , description                  :: T.Text
+  , accuracy                     :: T.Text
+  , profitable_ideas_avg_yield   :: Double
+  , total_profitable_ideas       :: Int
+  , unprofitable_ideas_avg_yield :: Double
+  , total_unprofitable_ideas     :: Int
+  , best_idea_id                 :: Int
+  , new_ideas_per_month          :: Int
+  , idea_avg_days_long           :: Int
+  , specialization_resume        :: SpecializationResume
   } deriving (Generic, Show)
 
 instance FromJSON Broker
+
+data SpecializationResume = SpecializationResume {
+    asset    :: Maybe T.Text
+  , currency :: Maybe T.Text
+  , txt      :: Maybe T.Text
+  } deriving (Generic, Show)
+
+instance FromJSON SpecializationResume
