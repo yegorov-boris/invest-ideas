e1f30b13bc574e980846f1a96fa17b810ec3c5fa
b.egorov <b.egorov@tinkoff.ru>
2019-06-13 14:32:36 +0300

    WIP Parse response body: happy path

--- src/Brokers/Client.hs
+++ src/Brokers/Client.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Brokers.Client
     (fetch
     ) where
 
-import Network.Http.Client (get, getStatusCode)
+import GHC.Generics (Generic)
+import Data.Aeson (FromJSON)
+--import Data.Time.LocalTime (ZonedTime)
+import Network.Http.Client (get, getStatusCode, jsonHandler)
 import Data.ByteString.UTF8 (fromString)
 import qualified Data.ByteString.Char8 as B
 import Control.Exception (SomeException, handle)
@@ -11,44 +16,15 @@ import Control.Concurrent (MVar, newEmptyMVar, readMVar, tryPutMVar, forkIO, thr
 import Flags (CliFlags, ideasURL, token, httpTimeout, httpMaxAttempts)
 import Utils (printWrap)
 
-fetch :: CliFlags -> IO ()
-fetch cf = attemptFetch cf 1
+data Body = Body {
+    success :: Bool
+  , results :: [Broker]
+  } deriving (Generic, Show)
 
-attemptFetch :: CliFlags -> Int -> IO ()
-attemptFetch cf currentAttempt = do
-  m <- newEmptyMVar
-  forkIO $ doFetch cf m currentAttempt
-  forkIO (threadDelay (httpTimeout cf) >> tryPutMVar m False >> return ())
-  success <- readMVar m
-  if'
-    (success || (currentAttempt == maxAttempts))
-    (return ())
-    (attemptFetch cf (succ currentAttempt))
-  where
-    maxAttempts = httpMaxAttempts cf
+instance FromJSON Body
 
-doFetch :: CliFlags -> MVar Bool -> Int -> IO ()
-doFetch cf m currentAttempt = do
-  printWrap "started fetching brokers, attempt " currentAttempt
-  handle (onErr currentAttempt) (get url processStatusCode)
-  where
-    url = fromString $ (ideasURL cf) ++ "/brokers?api_key=" ++ (token cf)
-    processStatusCode response inputStream = do
-      allowed <- tryPutMVar m True
-      if'
-        allowed
-        (let statusCode = getStatusCode response in if'
-          (statusCode == 200)
-          (printWrap "finished fetching brokers, attempt " currentAttempt)
-          (printWrap ("failed to fetch brokers, attempt " ++ show currentAttempt ++ ": status code ") statusCode))
-        (printWrap "failed to fetch brokers: timed out, attempt " currentAttempt)
-
-onErr :: Int -> SomeException -> IO ()
-onErr currentAttempt = printWrap ("failed to fetch brokers, attempt " ++ show currentAttempt ++ ": ")
-
---import Data.Time.LocalTime (ZonedTime)
-
---data Broker = Broker { externalID :: String
+data Broker = Broker {
+  id :: Int
 --                     , source :: String
 --                     , name :: String
 --                     , rating :: String
@@ -71,4 +47,48 @@ onErr currentAttempt = printWrap ("failed to fetch brokers, attempt " ++ show cu
 --                     , isDeleted :: Bool
 --                     , isVisibleMM :: Bool
 --                     , isVisibleWM :: Bool
---                     }
+  } deriving (Generic, Show)
+
+instance FromJSON Broker
+
+fetch :: CliFlags -> IO ()
+fetch cf = attemptFetch cf 1
+
+attemptFetch :: CliFlags -> Int -> IO ()
+attemptFetch cf currentAttempt = do
+  m <- newEmptyMVar
+  forkIO $ doFetch cf m currentAttempt
+  forkIO (threadDelay (httpTimeout cf) >> tryPutMVar m False >> return ())
+  success <- readMVar m
+  if'
+    (success || (currentAttempt == maxAttempts))
+    (return ())
+    (attemptFetch cf (succ currentAttempt))
+  where
+    maxAttempts = httpMaxAttempts cf
+
+-- TODO: chain the nested conditions
+doFetch :: CliFlags -> MVar Bool -> Int -> IO ()
+doFetch cf m currentAttempt = do
+  printWrap "started fetching brokers, attempt " currentAttempt
+  handle (onErr currentAttempt) (get url processStatusCode)
+  where
+    url = fromString $ (ideasURL cf) ++ "/brokers?api_key=" ++ (token cf)
+    processStatusCode response inputStream = do
+      allowed <- tryPutMVar m True
+      if'
+        allowed
+        (let statusCode = getStatusCode response in if'
+          (statusCode == 200)
+          (do
+            printWrap "finished fetching brokers, attempt " currentAttempt
+            printWrap "started parsing brokers response body, attempt " currentAttempt
+            body <- jsonHandler response inputStream :: IO Body -- TODO: on err
+            printWrap "finished parsing brokers response body, attempt " currentAttempt
+            putStrLn $ show body
+          )
+          (printWrap ("failed to fetch brokers, attempt " ++ show currentAttempt ++ ": status code ") statusCode))
+        (printWrap "failed to fetch brokers: timed out, attempt " currentAttempt)
+
+onErr :: Int -> SomeException -> IO ()
+onErr currentAttempt = printWrap ("failed to fetch brokers, attempt " ++ show currentAttempt ++ ": ")
