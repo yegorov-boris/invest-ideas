fe358a9c1b1b76a7159372b610159a3e0bd8cb03
b.egorov <b.egorov@tinkoff.ru>
2019-06-27 21:01:12 +0300

    Validate ideas

--- src/Ideas/Validator.hs
+++ src/Ideas/Validator.hs
@@ -2,10 +2,15 @@ module Ideas.Validator
     ( validate
     ) where
 
-import Data.HashSet (Set)
+import qualified Data.HashSet as HashSet
 import qualified Data.Text as T
+import Control.Conditional (if', select)
 import Ideas.Response (IdeaResponse(..))
 
--- TODO: implementation
-validate :: Set T.Text -> IdeaResponse -> Either String IdeaResponse
-validate stocksCache idea = Right idea
+validate :: HashSet.Set T.Text -> IdeaResponse -> Either String IdeaResponse
+validate stocksCache idea = do
+  i <- if' (isOpen idea) (Right $ idea {ticker = t}) (Left "idea is not open")
+  if' (not (T.null t) && ok t) (Right i) (Left "ticker not found")
+  where
+    ok = (`HashSet.member` stocksCache)
+    t  = select ok id (T.takeWhile (== '.')) (ticker idea)
